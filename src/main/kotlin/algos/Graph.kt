8d5d83bff1e73a7c6486e1cafaf9d23a51efd49a
bikakuka <bikakukavn@gmail.com>
2024-05-28 13:42:16 +0300

    added a template for graph class
    forget to commit it before

---
+++ src/main/kotlin/algos/Graph.kt
@@ -0,0 +1,27 @@
+package algos
+
+class Graph {
+    private val nodes = mutableListOf<Int>()
+    private val edges = mutableListOf<Pair<Int, Int>>()
+    private val adjacencyList = mutableMapOf<Int, MutableList<Int>>()
+
+    fun addNode(n: Int) {
+        TODO()
+    }
+
+    fun addEdge(n: Int, m: Int) {
+       TODO()
+    }
+    fun removeNode(n: Int) {
+        TODO()
+    }
+    fun removeEdge(n:Int, m:Int){
+        TODO()
+    }
+    fun findBridges(): List<Pair<Int, Int>> {
+        TODO()
+    }
+    fun printAll(){
+        TODO()
+    }
+}
