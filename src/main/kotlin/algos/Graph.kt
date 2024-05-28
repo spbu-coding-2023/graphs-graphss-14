563b7494f9f07133cdf13ceaf2035ba77105756b
Ruslan Nafikov <ruslan.nafikov.05@bk.ru>
2024-05-28 14:01:26 +0300

    Implemented a graph class and a bridge search algorithm

--- src/main/kotlin/algos/Graph.kt
+++ src/main/kotlin/algos/Graph.kt
@@ -6,22 +6,67 @@ class Graph {
     private val adjacencyList = mutableMapOf<Int, MutableList<Int>>()
 
     fun addNode(n: Int) {
-        TODO()
+        nodes.add(n)
+        adjacencyList[n] = mutableListOf()
     }
 
     fun addEdge(n: Int, m: Int) {
-       TODO()
+        if (Pair(n, m) !in edges) {
+            edges.add(Pair(n, m))
+            adjacencyList[n]?.add(m)
+            adjacencyList[m]?.add(n)
+        }
     }
     fun removeNode(n: Int) {
-        TODO()
+        nodes.remove(n)
+        adjacencyList.remove(n)
+        edges.removeAll { it.first == n || it.second == n }
+        for ((_, neighbors) in adjacencyList) {
+            neighbors.remove(n)
+        }
     }
     fun removeEdge(n:Int, m:Int){
-        TODO()
+        edges.remove(Pair(n,m))
+        edges.remove(Pair(m,n))
+        adjacencyList[n]?.remove(m)
+        adjacencyList[m]?.remove(n)
     }
     fun findBridges(): List<Pair<Int, Int>> {
-        TODO()
+        val bridges = mutableListOf<Pair<Int, Int>>()
+        val visited = mutableSetOf<Int>()
+        val discoveryTime = mutableMapOf<Int, Int>()
+        val low = mutableMapOf<Int, Int>()
+        var time = 0
+
+        fun dfs(node: Int, parent: Int) {
+            visited.add(node)
+            discoveryTime[node] = time
+            low[node] = time
+            time++
+
+            for (neighbor in adjacencyList[node] ?: emptyList()) {
+                if (!visited.contains(neighbor)) {
+                    dfs(neighbor, node)
+                    low[node] = minOf(low[node]!!, low[neighbor]!!)
+                    if (low[neighbor] == discoveryTime[neighbor]) {
+                        bridges.add(Pair(node, neighbor))
+                    }
+                } else if (neighbor != parent) {
+                    low[node] = minOf(low[node]!!, discoveryTime[neighbor]!!)
+                }
+            }
+        }
+
+        for (node in nodes) {
+            if (!visited.contains(node)) {
+                dfs(node, -1)
+            }
+        }
+
+        return bridges
     }
     fun printAll(){
-        TODO()
+        println(nodes)
+        println(edges)
     }
 }
